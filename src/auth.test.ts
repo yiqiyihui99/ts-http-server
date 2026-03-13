03446686eb993bee82b289860f5509c8ebf18262
yiqiyihui99 <ianwooduffy.99@gmail.com>
2026-03-12 18:25:37 -0700

    feat: added JWT generation and validation + tests. Upgraded node to 22.12.0 to allow running vitest

---
+++ src/auth.test.ts
@@ -0,0 +1,46 @@
+import { describe, it, expect, beforeAll } from "vitest";
+import { checkPasswordHash, hashPassword, makeJWT, validateJWT } from "../src/auth";
+import { UnauthorizedError } from "../src/api/errors";
+
+describe("Password Hashing", () =>
+{
+    const password1 = "correctPassword123!";
+    const password2 = "incorrectPassword123!";
+    let hash1: string;
+    let hash2: string;
+
+    beforeAll(async () => {
+        hash1 = await hashPassword(password1);
+        hash2 = await hashPassword(password2);
+    });
+
+    it("should return true for the correct password", async () => {
+        const result = await checkPasswordHash(password1, hash1);
+        expect(result).toBe(true);
+    });
+
+    it("should return false for the incorrect hash/password", async () => {
+        const result = await checkPasswordHash(password1, hash2);
+        expect(result).toBe(false);
+    });
+});
+
+describe("JWT", () => {
+    const userID1 = "df7c3d50-8959-491b-9dc1-9902474b47f0";
+    const secret1 = "qv#*SMBnAqK_iMhbxN[>GQ+!+AWC$dt1";
+    const secret2 = "qv#*SMBnAqK_iMhbxN[>GQ+!+AWC$dt2";
+    let token1: string;
+
+    beforeAll(async () => {
+        token1 = makeJWT(userID1, 3600, secret1);
+    });
+
+    it("should return the userID for the correct token", () => {
+        const result = validateJWT(token1, secret1);
+        expect(result).toBe(userID1);
+    });
+
+    it("should throw an error for the incorrect secret", () => {
+        expect(() => validateJWT(token1, secret2)).toThrow(UnauthorizedError);
+    });
+});
--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,9 @@
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
+import type { JwtPayload } from "jsonwebtoken";
+import { UnauthorizedError } from "./api/errors";
+
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
 
 export async function hashPassword(password: string): Promise<string> {
     return await argon2.hash(password);
@@ -9,4 +14,30 @@ export async function checkPasswordHash(
   hash: string,
 ): Promise<boolean> {
     return await argon2.verify(hash, password);
+}
+
+
+// .sign() and .verify() are synchronous so no need to make async or handle w/ try/catch
+export function makeJWT(userID: string, expiresIn: number, secret: string): string {
+  const currTime = Math.floor(Date.now() / 1000);
+  const payload: payload = {
+    iss: "chirpy",
+    sub: userID,
+    iat: currTime,
+    exp: currTime + expiresIn,
+  };
+
+  return jwt.sign(payload, secret);
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+  try {
+    const verified = jwt.verify(tokenString, secret) as JwtPayload;
+    if (!verified.sub) {
+      throw new UnauthorizedError("Invalid token");
+    }
+    return verified.sub;
+  } catch (e) {
+    throw new UnauthorizedError("Invalid token");
+  } 
 }
